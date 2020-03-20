f86df662cd8b5b1b8dfccb3af77bb88a5ec0972e
potato <pot@to>
2020-03-20 22:42:10 +0200

    server and api are working

---
+++ src/api.py
@@ -0,0 +1,144 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import ciso8601
+import socket
+import json
+import time
+
+SERVER_HOST = ''
+SERVER_PORT = '8086'
+API_HOST = '0.0.0.0'
+API_PORT = '8085'
+
+data_template = pd.DataFrame(columns=['id', 'reminder_time', 'message', 'x_sec_repeat'])
+data_file = f"data.csv"
+
+app = Flask(__name__)
+
+
+# creates missing file
+def create_files():
+    # data file
+    try:
+        f = open(data_file, "r")
+        f.close()
+    except FileNotFoundError:
+        df = data_template.copy()
+        df.to_csv(data_file, index=False)
+
+
+# sends data to the server
+def send_to_server(df):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((SERVER_HOST, int(SERVER_PORT)))
+    msg = df.to_json()
+    client.send(msg.encode('utf-8'))
+    client.close()
+
+
+# returns thhe ID of the last created reminder, or 0 if empty
+def get_last_id():
+    data = pd.read_csv(data_file)
+    last_id = 0
+    if not data.empty:
+        last_id = data['id'].max()
+
+    return last_id
+
+
+# parses time to epoch format
+def parse_time(dt):
+    ts = ciso8601.parse_datetime(dt)
+    ts = int(time.mktime(ts.timetuple()))
+    return ts
+
+
+# returns a dataframe containing all or part of the reminders
+def get_reminder(from_date, to_date, id_list):
+    data = pd.read_csv(data_file)
+
+    if id_list is not None:
+        id_list = id_list.split(",")
+        id_list_df = {'id': id_list}
+        data = data[data.id.isin(id_list_df)]
+
+    if from_date is not None:
+        dt = parse_time(from_date)
+        data = data.loc[data['reminder_time'] > dt]
+
+    if to_date is not None:
+        dt = parse_time(to_date)
+        data = data.loc[data['reminder_time'] < dt]
+
+    return data
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/add', methods=['POST'])
+def add_reminder():
+    global next_id
+    msg = request.form['message']
+    dt = request.form['time']
+    dt = parse_time(dt)
+
+    new_rem_df = data_template.copy()
+    new_row = {'id': next_id, 'reminder_time': dt, 'message': msg, 'x_sec_repeat': 0}
+    new_rem_df = new_rem_df.append(new_row, ignore_index=True)
+
+    # save to the data file
+    new_rem_df.to_csv(data_file, mode='a', header=False, index=False)
+    next_id += 1
+
+    # update the server
+    if new_rem_df.iloc[0]['reminder_time'] >= int(time.time()):
+        new_rem_df['mode'] = 'a'
+        send_to_server(new_rem_df)
+
+    return ('added')
+    # TODO: return status code
+
+
+@app.route('/remove', methods=['POST'])
+def remove_rem():
+    from_date = request.form('from')
+    to_date = request.form('to')
+    id_list = request.form('id')
+
+    # create a df of IDs to delete
+    rm_id = get_reminder(from_date, to_date, id_list)
+    rm_id = rm_id['id']
+
+    # update data file
+    data = pd.read_csv(data_file)
+    data = data[~data.id.isin(rm_id)]
+    data.to_csv(data_file, header=True, index=False)
+
+    # update server
+    rm_id['mode'] = 'r'
+    send_to_server(rm_id)
+
+    jdata = rm_id.to_json()
+
+    return jdata
+
+
+@app.route('/list', methods=['GET'])
+def get_rem():
+    from_date = request.args.get('from')
+    to_date = request.args.get('to')
+    id_list = request.args.get('id')
+
+    data = get_reminder(from_date, to_date, id_list)
+    jdata = json.loads(data.to_json(orient='records'))
+    return jsonify(jdata)
+
+
+if __name__ == '__main__':
+    create_files()
+    next_id = get_last_id() + 1
+
+    app.run(debug=True, host=API_HOST, port=API_PORT)
---
+++ src/server.py
@@ -0,0 +1,107 @@
+from pygame import mixer
+from gtts import gTTS
+import pandas as pd
+import threading
+import requests
+import socket
+import time
+
+
+SERVER_HOST = ''
+SERVER_PORT = '8086'
+API_HOST = '0.0.0.0'
+API_PORT = '8085'
+
+data_template = pd.DataFrame(columns=['id', 'reminder_time', 'message', 'x_sec_repeat'])
+
+
+# loads relevant data on app startup (only upcoming reminders)
+def load_data():
+    global data
+    url = f'http://{API_HOST}:{API_PORT}/list'
+    params = {'from': time.strftime("%Y%m%dt%H%M%S", time.localtime(time.time()))}
+    r = requests.get(url=url, params=params)
+
+    data = pd.read_json(r.content, orient="records",  convert_dates=False)
+    if data.empty:
+        data = data_template.copy()
+    data = data.sort_values(by=['reminder_time'])
+
+
+# creates and plays the recording
+def play_rec(msg):
+    test_t = time.time()
+    filename = "rec" + ".mp3"
+    language = 'en'
+
+    # create a record file of the reminder and save locally
+    myobj = gTTS(text=msg, lang=language, slow=False)
+    myobj.save(filename)
+    print(f'create rec: {time.time() - test_t}')
+    # play the recording
+    mixer.init()
+    mixer.music.load(filename)
+    mixer.music.play()
+    #while mixer.music.get_busy():
+    #    time.sleep(0.1)
+
+    print(f'play: {time.time() - test_t}')
+    #TODO: delete rec file, maybe delete while
+
+
+# adds or removes reminders in data df
+def update(df):
+    global data
+
+    mode = df.iloc[0]['mode']
+    df = df.drop(['mode'], axis=1)
+
+    # add upcoming reminders to data df
+    if mode == 'a':
+        data = data.append(df, ignore_index=True)
+        data = data.sort_values(by=['reminder_time'])
+    # delete reminder
+    elif mode == 'd':
+        data = data[~data.id.isin(df['id'])]
+
+
+# receives updates through socket
+def server_socket():
+    global data
+
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((SERVER_HOST, int(SERVER_PORT)))
+    serversocket.listen(5)
+    while True:
+        conn, address = serversocket.accept()
+        while True:
+            received_data = conn.recv(4096)
+            if not received_data:
+                break
+            new_data = received_data.decode('utf-8')
+            new_data = pd.read_json(new_data, convert_dates=False)
+            update(new_data)
+        conn.close()
+
+
+# plays reminders
+def play_reminder():
+    global data
+
+    while True:
+        if not data.empty:
+            if int(data.iloc[0]["reminder_time"]) <= int(time.time()):
+                msg = data.iloc[0]["message"]
+                print(msg)
+                play_rec(msg)
+                data = data.drop(data.index[0])
+
+
+if __name__ == '__main__':
+    data = None
+    socket_thread = threading.Thread(target=server_socket, args=())
+
+    load_data()
+    socket_thread.start()
+    play_reminder()
+
